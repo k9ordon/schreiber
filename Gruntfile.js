6b1dc790b254ce7420ed5b8971c0d8f66febbec1
Klemens Gordon <github@k94n.com>
2013-10-02 15:09:06 +0200

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+        options: {
+            separator: ';\n'
+        },
+        dist: {
+            files: {
+                'dist/app.js': [
+                    // libs
+                    'vendor/CodeMirror/lib/codemirror.js',
+                    'vendor/CodeMirror/addon/edit/continuelist.js',
+                    'vendor/CodeMirror/mode/xml/xml.js',
+                    'vendor/CodeMirror/mode/markdown/markdown.js',
+                    // app
+                    'js/file.js', 
+                    'js/files.js', 
+                    'js/app.js', 
+                     // boot
+                    'js/boot.js',
+                    'vendor/gapi-chrome-apps.js'
+                ],
+                'dist/chrome.js': ['js/chrome.js']
+            }
+        }
+    },
+    less: {
+        dist: {
+            files: {
+                "dist/app.css": "less/app.less"
+            },
+            options: {
+                strictImports: true
+            }
+        }
+    },
+    copy: {
+        dist: {
+            files: [
+                {src: ['vendor/linecons/fonts/*'], dest: 'dist/fonts/', flatten: true}
+            ]
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', ['concat', 'less', 'copy']);
+};
