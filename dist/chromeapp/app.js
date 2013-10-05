4a2bef4e43e8475390bc3d6210bdc6b41ff667fb
Klemens Gordon <github@k94n.com>
2013-10-05 19:27:44 +0200

    added preview cursor

--- dist/chromeapp/app.js
+++ dist/chromeapp/app.js
@@ -10901,12 +10901,41 @@ p.events = function() {
 };
 
 p.update = function() {
-    console.log('preview update', app.file.preview.$el);
+    console.log('preview update');
 
-    var html = marked(app.file.editor.getValue());
-    app.file.preview.$el.innerHTML = html;
+    var html = app.file.editor.getValue(),
+        html_cursor = app.file.preview.insertEditorCursorPositon(html);
+
+    app.file.preview.$el.innerHTML = marked(html_cursor);
 };
-;
+
+p.insertEditorCursorPositon = function(text) {
+    var position = app.file.editor.getCursor(),
+        newText = '',
+        lines = text.split(/\n/);
+
+    console.log('insertEditorCursorPositon', position, text.split(/\n/));
+
+    for(var i = 0; i < lines.length; i++) {
+        if(i === position.line) {
+            
+            var cLine = lines[i];
+                newline = cLine.substring(0, position.ch) + 
+                "|<div id='cursor'></div>" + 
+                cLine.substring(position.ch, cLine.length) + 
+                "\n";
+
+            console.log('line:' + newline);
+            newText += newline;            
+        } else {
+            newText += lines[i] + "\n";
+        }
+    }
+
+    //console.log('new text' + newText);
+
+    return newText;
+};;
 var File = function() {
         this.$template = document.querySelector('#fileTemplate');
         this.$welcomefileTemplate = document.querySelector('#welcomefileTemplate');
