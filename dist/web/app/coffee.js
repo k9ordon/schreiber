decc64ccb5adeb5cbf73970704c40534b7381410
Klemens Gordon <github@k94n.com>
2013-10-09 23:48:30 +0200

    rebuild

--- dist/web/app/coffee.js
+++ dist/web/app/coffee.js
@@ -1,23 +1,27 @@
 (function() {
-  var App, Titlebar, app;
+  var App, Document, Titlebar;
 
   Titlebar = (function() {
     function Titlebar(app) {
       this.app = app;
-      console.log('Titlebar init');
       this.dom();
       this.events();
     }
 
     Titlebar.prototype.dom = function() {
       this.$el = document.querySelector('.titlebar');
-      return this.$title = this.$el.querySelector('.title');
+      this.$title = this.$el.querySelector('.title');
+      this.$toPreview = this.$el.querySelector('#toPreview');
+      return this.$toDocuments = this.$el.querySelector('#toDocuments');
     };
 
     Titlebar.prototype.events = function() {
       var _this = this;
-      return this.$el.addEventListener('mouseover', function(e) {
-        return console.log('titlebar hover');
+      this.$toPreview.addEventListener('click', function(e) {
+        return _this.app.showPreview();
+      });
+      return this.$toDocuments.addEventListener('click', function(e) {
+        return _this.app.showDocuments();
       });
     };
 
@@ -25,16 +29,110 @@
 
   })();
 
+  Document = (function() {
+    function Document(app, driveId, title, text) {
+      this.app = app;
+      this.driveId = driveId;
+      this.title = title;
+      this.text = text;
+      console.log(this.app.$el);
+      this.create();
+      this.dom();
+      this.createCodeMirror();
+    }
+
+    Document.prototype.dom = function() {
+      this.$src = this.$el.querySelector('#src');
+      return this.$preview = this.$el.querySelector('#preview');
+    };
+
+    Document.prototype.create = function() {
+      var t, tpl;
+      tpl = document.querySelector('#fileTemplate');
+      t = document.createElement('div');
+      t.innerHTML = tpl.innerHTML;
+      this.$el = t.firstChild;
+      return this.app.$el.appendChild(this.$el);
+    };
+
+    Document.prototype.createCodeMirror = function() {
+      var config;
+      config = {
+        mode: 'gfm',
+        lineNumbers: false,
+        theme: "schreiber",
+        lineWrapping: true,
+        styleSelectedText: true,
+        styleActiveLine: true,
+        extraKeys: {
+          "Enter": "newlineAndIndentContinueMarkdownList"
+        },
+        viewportMargin: 1000
+      };
+      this.cm = CodeMirror.fromTextArea(this.$src, config);
+      return this.cm.setValue(this.text);
+    };
+
+    Document.prototype.events = function() {};
+
+    Document.prototype.show = function() {
+      this.app.d.$el.id = '';
+      this.$el.id = 'currentFile';
+      this.app.d.cm.refresh();
+      return this.app.d.cm.focus();
+    };
+
+    return Document;
+
+  })();
+
   App = (function() {
     function App() {
-      this.titlebar = new Titlebar(App);
+      this.dom();
+      this.sub();
+      this.show();
+      this.openFile(false, 'welcome.md', 'welcome');
     }
+
+    App.prototype.dom = function() {
+      this.$el = document.querySelector('#app');
+      return this.$loading = document.querySelector('#loading');
+    };
+
+    App.prototype.sub = function() {
+      this.titlebar = new Titlebar(this);
+      return this.documents = [];
+    };
+
+    App.prototype.show = function() {
+      this.$el.classList.remove('hidden');
+      return this.$loading.classList.add('hidden');
+    };
+
+    App.prototype.showDocuments = function() {
+      return console.log('showDocuments');
+    };
+
+    App.prototype.showPreview = function() {
+      return console.log('showPreview');
+    };
+
+    App.prototype.openFile = function(dId, title, text) {
+      this.dId = dId;
+      this.title = title;
+      this.text = text;
+      this.d = new Document(this, this.dId, this.title, this.text);
+      this.documents.push(this.d);
+      return this.d.show();
+    };
 
     return App;
 
   })();
 
-  app = new App;
+  setTimeout((function() {
+    return window.app = new App;
+  }), 100);
 
 }).call(this);
 
--- dist/web/app/coffee.src.coffee
+++ dist/web/app/coffee.src.coffee
@@ -1,17 +1,91 @@
 class Titlebar
+
     constructor: (@app) ->
-        console.log 'Titlebar init'
         @dom()
         @events()
 
     dom: ->
         @$el = document.querySelector '.titlebar'
         @$title = @$el.querySelector '.title'
+        @$toPreview = @$el.querySelector '#toPreview'
+        @$toDocuments = @$el.querySelector '#toDocuments'
 
     events: ->
-        @$el.addEventListener 'mouseover', (e) => console.log 'titlebar hover'
+        @$toPreview.addEventListener 'click', (e) => @app.showPreview()
+        @$toDocuments.addEventListener 'click', (e) => @app.showDocuments()
+
+class Document
+
+    constructor: (@app, @driveId, @title, @text) ->
+        console.log @app.$el
+ 
+        @create()
+        @dom()
+        @createCodeMirror()
+
+    dom: ->
+        @$src = @$el.querySelector('#src')
+        @$preview = @$el.querySelector('#preview')        
+        
+    create: ->
+        tpl = document.querySelector '#fileTemplate'
+        t = document.createElement 'div'
+        t.innerHTML = tpl.innerHTML
+        @$el = t.firstChild
+        @app.$el.appendChild @$el
+
+    createCodeMirror: ->
+        config = 
+            mode: 'gfm'
+            lineNumbers: false
+            theme: "schreiber"
+            lineWrapping: true
+            styleSelectedText: true
+            styleActiveLine: true
+            extraKeys: 
+                "Enter": "newlineAndIndentContinueMarkdownList"
+            viewportMargin: 1000
+
+        @cm = CodeMirror.fromTextArea @$src, config
+        @cm.setValue @text
+
+    events: ->
+
+    show: ->
+        @app.d.$el.id = ''
+        @$el.id = 'currentFile'
+
+        @app.d.cm.refresh()
+        @app.d.cm.focus()
 class App
 
     constructor: ->
-        @titlebar = new Titlebar App
-app = new App
+        @dom() 
+        @sub() 
+        @show()
+        @openFile(false, 'welcome.md', 'welcome')
+
+    dom: ->
+        @$el = document.querySelector '#app'
+        @$loading = document.querySelector '#loading'
+
+    sub: ->
+        @titlebar = new Titlebar @
+        @documents = []
+
+    show: ->
+        @$el.classList.remove 'hidden'
+        @$loading.classList.add 'hidden'        
+
+    showDocuments: ->
+        console.log 'showDocuments'
+
+    showPreview: ->
+        console.log 'showPreview'
+
+    openFile: (@dId, @title, @text) ->
+        @d = new Document @, @dId, @title, @text
+        @documents.push @d
+        @d.show()
+
+setTimeout (-> window.app = new App), 100
