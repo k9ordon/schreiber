0559ab2b22626ec3c8b0608ce315f4d382d21772
Klemens Gordon <github@k94n.com>
2013-10-28 11:57:27 +0100

    rebuild

--- dist/web/app/coffee.js
+++ dist/web/app/coffee.js
@@ -1,5 +1,5 @@
 (function() {
-  var App, Document, Documents, Drive, Preview, Titlebar;
+  var App, Document, Documents, Info, Preview, Slides, Titlebar;
 
   Titlebar = (function() {
     function Titlebar(app) {
@@ -12,81 +12,147 @@
       this.$el = document.querySelector('.titlebar');
       this.$title = this.$el.querySelector('.title');
       this.$toPreview = this.$el.querySelector('#toPreview');
+      this.$toSlides = this.$el.querySelector('#toSlides');
       return this.$toDocuments = this.$el.querySelector('#toDocuments');
     };
 
     Titlebar.prototype.events = function() {
-      var _this = this;
-      this.$toPreview.addEventListener('click', function(e) {
-        return _this.app.showPreview();
-      });
-      return this.$toDocuments.addEventListener('click', function(e) {
-        return _this.app.showDocuments();
-      });
+      this.$el.addEventListener('mouseover', this.app.distractionFreeLeave);
+      this.$toPreview.addEventListener('click', this.app.togglePreview);
+      this.$toSlides.addEventListener('click', this.app.toggleSlides);
+      return this.$toDocuments.addEventListener('click', this.app.toggleDocuments);
+    };
+
+    Titlebar.prototype.setTitle = function(filename) {
+      return this.$title.innerHTML = filename;
     };
 
     return Titlebar;
 
   })();
 
-  Drive = (function() {
-    Drive.auth_params = {
-      'client_id': '140224327941.apps.googleusercontent.com',
-      'scope': ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/userinfo.email']
-    };
-
-    function Drive() {
-      this.authorize();
-    }
-
-    Drive.prototype.authorize = function() {
-      return gapi.auth.authorize(this.auth_params, function(auth) {
-        return console.log(auth);
-      });
-    };
-
-    return Drive;
-
-  })();
-
   Documents = (function() {
     function Documents(app) {
       this.app = app;
-      events();
+      this.instances = [];
+      this.dom();
+      this.events();
     }
 
-    Documents.prototype.events = function() {};
+    Documents.prototype.dom = function() {
+      return this.$el = document.querySelector('#fileBrowser');
+    };
+
+    Documents.prototype.events = function() {
+      return this.$el.addEventListener('mouseover', this.app.distractionFreeLeave);
+    };
+
+    Documents.prototype.add = function(documentInstance) {
+      return this.instances.push(documentInstance);
+    };
+
+    Documents.prototype.get = function(idx) {
+      return this.instances[idx];
+    };
 
     return Documents;
 
   })();
 
-  Preview = (function() {
-    function Preview(app, file) {
+  Slides = (function() {
+    function Slides(app, d) {
       this.app = app;
-      this.file = file;
+      this.d = d;
       this.dom();
+      this.events();
+    }
+
+    Slides.prototype.dom = function() {
+      return this.$el = this.d.$el.querySelector('.slides');
+    };
+
+    Slides.prototype.events = function() {
+      return this.$el.addEventListener('mouseover', this.app.distractionFreeLeave);
+    };
+
+    Slides.prototype.update = function() {
+      var htmlSlides, markup, markupSlides, _i, _len;
+      markupSlides = this.d.getValueWithCursor().split("\n---\n");
+      htmlSlides = '';
+      for (_i = 0, _len = markupSlides.length; _i < _len; _i++) {
+        markup = markupSlides[_i];
+        htmlSlides += '<div>' + marked(markup + '</div>');
+      }
+      return this.$el.innerHTML = htmlSlides;
+    };
+
+    Slides.prototype.updateScrollPosition = function() {
+      return this.$el.scrollTop = this.$el.querySelector('#cursor').offsetTop - this.$el.offsetHeight / 2;
+    };
+
+    return Slides;
+
+  })();
+
+  Preview = (function() {
+    function Preview(app, d) {
+      this.app = app;
+      this.d = d;
+      this.dom();
+      this.events();
     }
 
     Preview.prototype.dom = function() {
-      return this.$el = this.file.$el.querySelector('.preview');
+      return this.$el = this.d.$el.querySelector('.preview');
+    };
+
+    Preview.prototype.events = function() {
+      return this.$el.addEventListener('mouseover', this.app.distractionFreeLeave);
     };
 
     Preview.prototype.update = function() {
-      return console.log('preview upadte');
+      return this.$el.innerHTML = marked(this.d.getValueWithCursor());
+    };
+
+    Preview.prototype.updateScrollPosition = function() {
+      return this.$el.scrollTop = this.$el.querySelector('#cursor').offsetTop - this.$el.offsetHeight / 2;
     };
 
     return Preview;
 
   })();
 
-  Document = (function() {
-    function Document(app, driveId, title, text) {
+  Info = (function() {
+    function Info(app, d) {
       this.app = app;
-      this.driveId = driveId;
-      this.title = title;
-      this.text = text;
-      console.log(this.app.$el);
+      this.d = d;
+      this.dom();
+      this.events();
+    }
+
+    Info.prototype.dom = function() {
+      return this.$el = this.d.$el.querySelector('.info');
+    };
+
+    Info.prototype.events = function() {
+      return this.$el.addEventListener('mouseover', this.app.distractionFreeLeave);
+    };
+
+    Info.prototype.update = function() {
+      var tokens;
+      tokens = markdown.parse(this.d.cm.getValue());
+      return this.$el.innerHTML = '<pre>' + JSON.stringify(tokens, void 0, 2) + '</pre>';
+    };
+
+    return Info;
+
+  })();
+
+  Document = (function() {
+    function Document(app, InkBlob) {
+      this.app = app;
+      this.InkBlob = InkBlob;
+      console.log(this.app.$el, this.InkBlob);
       this.create();
       this.sub();
       this.dom();
@@ -104,7 +170,9 @@
     };
 
     Document.prototype.sub = function() {
-      return this.preview = new Preview(this.app, this);
+      this.info = new Info(this.app, this);
+      this.preview = new Preview(this.app, this);
+      return this.slides = new Slides(this.app, this);
     };
 
     Document.prototype.dom = function() {
@@ -119,6 +187,7 @@
         lineNumbers: false,
         theme: "schreiber",
         lineWrapping: true,
+        matchBrackets: true,
         styleSelectedText: true,
         styleActiveLine: true,
         extraKeys: {
@@ -127,13 +196,27 @@
         viewportMargin: 1000
       };
       this.cm = CodeMirror.fromTextArea(this.$src, config);
-      return this.cm.setValue(this.text);
+      if (this.text) {
+        return this.cm.setValue(this.text);
+      }
     };
 
     Document.prototype.events = function() {
-      return this.cm.on("change", function() {
-        this.app.setDistractionFree(true);
-        return this.app.d.preview.update();
+      this.cm.on("change", function() {
+        this.app.distractionFreeEnter();
+        this.app.d.info.update();
+        this.app.d.preview.update();
+        this.app.d.preview.updateScrollPosition();
+        return this.app.d.slides.update();
+      });
+      this.cm.on("cursorActivity", function() {
+        this.app.d.info.update();
+        this.app.d.preview.update();
+        this.app.d.preview.updateScrollPosition();
+        return this.app.d.slides.update();
+      });
+      return this.cm.on("blur", function() {
+        return this.app.distractionFreeLeave();
       });
     };
 
@@ -141,7 +224,33 @@
       this.app.d.$el.id = '';
       this.$el.id = 'currentFile';
       this.app.d.cm.refresh();
-      return this.app.d.cm.focus();
+      this.app.d.cm.focus();
+      return this.app.titlebar.setTitle(this.title);
+    };
+
+    Document.prototype.save = function(e) {
+      e.preventDefault();
+      return alert('save');
+    };
+
+    Document.prototype.getValueWithCursor = function() {
+      var cursor, cursorText, line, lines, text, _i, _len;
+      text = this.cm.getValue();
+      cursor = this.cm.getCursor();
+      lines = text.split("\n");
+      cursorText = '';
+      for (line = _i = 0, _len = lines.length; _i < _len; line = ++_i) {
+        text = lines[line];
+        if (line === cursor.line) {
+          cursorText += text.substring(0, cursor.ch);
+          cursorText += "|<span id='cursor'></span>";
+          cursorText += text.substring(cursor.ch, text.length);
+          cursorText += "\n";
+        } else {
+          cursorText += text + "\n";
+        }
+      }
+      return cursorText;
     };
 
     return Document;
@@ -153,20 +262,30 @@
       this.dom();
       this.sub();
       this.show();
+      this.events();
+      filepicker.setKey("ArBbG8nVdT6esL691Pkdvz");
     }
 
     App.prototype.dom = function() {
       this.$el = document.querySelector('#app');
-      return this.$loading = document.querySelector('#loading');
+      this.$loading = document.querySelector('#loading');
+      return this.$openPicker = document.querySelector('#openPickerButton');
     };
 
     App.prototype.sub = function() {
       this.titlebar = new Titlebar(this);
-      return this.documents = [];
+      return this.documents = new Documents(this);
+    };
+
+    App.prototype.events = function() {
+      Mousetrap.bindGlobal('command+s', this.d.save);
+      Mousetrap.bindGlobal('command+p', this.togglePreview);
+      Mousetrap.bindGlobal('command+p', this.openPicker);
+      return this.$openPicker.addEventListener('click', this.openPicker);
     };
 
     App.prototype.show = function() {
-      this.openFile(false, 'welcome.md', 'welcome');
+      this.openDocument(false);
       this.$el.classList.remove('hidden');
       return this.$loading.classList.add('hidden');
     };
@@ -175,24 +294,53 @@
       return console.log('showDocuments');
     };
 
-    App.prototype.showPreview = function() {
-      return console.log('showPreview');
+    App.prototype.openPicker = function() {
+      return filepicker.pick({
+        container: 'modal',
+        extensions: ['.md', '.markdown', '.txt'],
+        services: ['GOOGLE_DRIVE', 'GITHUB', 'DROPBOX', 'COMPUTER', 'FTP', 'WEBDAV']
+      }, function(InkBlob) {
+        console.log(JSON.stringify(InkBlob));
+        return app.openDocument(JSON.stringify(InkBlob));
+      }, function(FPError) {
+        return console.log(FPError.toString());
+      });
     };
 
-    App.prototype.openFile = function(dId, title, text) {
-      this.dId = dId;
-      this.title = title;
-      this.text = text;
-      this.d = new Document(this, this.dId, this.title, this.text);
-      this.documents.push(this.d);
+    App.prototype.togglePreview = function(e) {
+      e.preventDefault();
+      if (!document.body.classList.contains('preview')) {
+        console.log('showPreview');
+        return document.body.classList.add('preview');
+      } else {
+        console.log('hidePreview');
+        return document.body.classList.remove('preview');
+      }
+    };
+
+    App.prototype.toggleSlides = function(e) {
+      e.preventDefault();
+      if (!document.body.classList.contains('slides')) {
+        console.log('showSlides');
+        return document.body.classList.add('slides');
+      } else {
+        console.log('hideSlides');
+        return document.body.classList.remove('slides');
+      }
+    };
+
+    App.prototype.openDocument = function(InkBlob) {
+      this.d = new Document(this, InkBlob);
+      this.documents.add(this.d);
       return this.d.show();
     };
 
-    App.prototype.setDistractionFree = function(bool) {
-      this.bool = bool;
-      if (this.bool) {
-        return document.body.classList.add('distractionFree');
-      }
+    App.prototype.distractionFreeEnter = function() {
+      return document.body.classList.add('distractionFree');
+    };
+
+    App.prototype.distractionFreeLeave = function() {
+      return document.body.classList.remove('distractionFree');
     };
 
     App.prototype.onGapiReady = function() {
